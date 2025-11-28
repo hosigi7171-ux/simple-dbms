ebb9f1fde0e36e534cd733bc2d582c469e0d4fe9
hosigi7171-ux <hosigi7171@gmail.com>
2025-11-28 15:13:00 +0900

    feat: implement buffer management layer
    - implement buffer managment layer
    - set common constants in common config header

---
+++ bpt/include/buf_mgr.h
@@ -0,0 +1,71 @@
+#ifndef BUF_MGR_H
+#define BUF_MGR_H
+
+#include "common_config.h"
+#include "page.h"
+#include <unordered_map>
+
+#define PREFETCH_SIZE 4
+
+/**
+ * dto for make_and_pin_page
+ */
+typedef struct {
+  page_t *page_ptr;
+  pagenum_t page_num;
+} allocated_page_info_t;
+
+typedef struct {
+  void *frame;
+  tableid_t table_id;
+  pagenum_t page_num;
+  bool is_dirty;
+  int pin_count;
+  bool ref_bit;
+  // fields will be added later
+} buf_ctl_block_t;
+
+typedef struct {
+  buf_ctl_block_t *frames;
+  int frames_size;
+  int clock_hand;
+  std::unordered_map<pagenum_t, frame_idx_t> page_table[MAX_TABLE_COUNT + 1];
+} buffer_manager_t;
+
+extern buffer_manager_t buf_mgr;
+
+// flush buffer
+void flush_table_buffer(int fd, tableid_t table_id);
+void flush_all_buffers(void);
+void flush_frame(int fd, tableid_t table_id, frame_idx_t frame_idx);
+
+// read/write buffer
+header_page_t *read_header_page(int fd, tableid_t table_id);
+page_t *
+get_page_from_buffer(pagenum_t page_num,
+                     std::unordered_map<pagenum_t, frame_idx_t> &frame_mapper);
+page_t *read_buffer(int fd, tableid_t table_id, pagenum_t page_num);
+frame_idx_t load_page_into_buffer(int fd, tableid_t table_id,
+                                  pagenum_t page_num);
+void set_new_bcb(tableid_t table_id, pagenum_t page_num, frame_idx_t frame_idx,
+                 page_t *page_buf);
+void set_new_prefetched_bcb(tableid_t table_id, pagenum_t page_num,
+                            frame_idx_t frame_idx, page_t *page_buf);
+void prefetch(int fd, pagenum_t page_num, tableid_t table_id,
+              frame_idx_t frame_idx,
+              std::unordered_map<pagenum_t, frame_idx_t> &frame_mapper);
+void write_buffer(tableid_t table_id, pagenum_t page_num, page_t *page);
+allocated_page_info_t make_and_pin_page(int fd, tableid_t table_id);
+
+// set pin count
+void pin(tableid_t table_id, pagenum_t page_num);
+void pin_frame(frame_idx_t frame_idx);
+void unpin(tableid_t table_id, pagenum_t page_num);
+void unpin_frame(frame_idx_t frame_idx);
+
+// clock algorithm
+void update_clock_hand(void);
+frame_idx_t find_free_frame_index(int fd, tableid_t table_id,
+                                  pagenum_t page_num);
+
+#endif
---
+++ bpt/src/buf_mgr.cpp
@@ -0,0 +1,245 @@
+#include "buf_mgr.h"
+#include "file.h"
+#include <cstring>
+#include <unordered_map>
+
+buffer_manager_t buf_mgr = {0}; // temp buffer manager
+
+/**
+ * flush buffer-----------------------------------------------------
+ */
+
+void flush_table_buffer(int fd, tableid_t table_id) {
+  std::unordered_map<pagenum_t, frame_idx_t> &frame_mapper =
+      buf_mgr.page_table[table_id];
+
+  for (auto it = frame_mapper.begin(); it != frame_mapper.end(); it++) {
+    frame_idx_t fidx = it->second;
+    flush_frame(fd, table_id, fidx);
+  }
+}
+
+void flush_frame(int fd, tableid_t table_id, frame_idx_t frame_idx) {
+  buf_ctl_block_t *bcb = &buf_mgr.frames[frame_idx];
+
+  if (bcb->is_dirty && bcb->pin_count == 0) {
+    file_write_page(fd, bcb->page_num, (page_t *)bcb->frame);
+    bcb->is_dirty = false;
+    bcb->ref_bit = false;
+    bcb->frame = NULL;
+    bcb->page_num = 0;
+    bcb->table_id = 0;
+  }
+}
+
+/**
+ * read/write buffer-----------------------------------------------------
+ */
+
+page_t *
+get_page_from_buffer(pagenum_t page_num,
+                     std::unordered_map<pagenum_t, frame_idx_t> &frame_mapper) {
+  frame_idx_t frame_idx = frame_mapper[page_num];
+  buf_ctl_block_t *bcb = &buf_mgr.frames[frame_idx];
+
+  pin_frame(frame_idx);
+
+  return (page_t *)bcb->frame;
+}
+
+page_t *read_buffer(int fd, tableid_t table_id, pagenum_t page_num) {
+  std::unordered_map<pagenum_t, frame_idx_t> &frame_mapper =
+      buf_mgr.page_table[table_id];
+
+  // Case: if page exists in buffer
+  if (frame_mapper.count(page_num)) {
+    return get_page_from_buffer(page_num, frame_mapper);
+  }
+
+  // Case: not exsits, read page from disk and write buffer
+  frame_idx_t frame_idx = load_page_into_buffer(fd, table_id, page_num);
+
+  prefetch(fd, page_num, table_id, frame_idx, frame_mapper);
+
+  return (page_t *)buf_mgr.frames[frame_idx].frame;
+}
+
+void write_buffer(tableid_t table_id, pagenum_t page_num, page_t *page) {
+  frame_idx_t frame_idx = buf_mgr.page_table[table_id][page_num];
+  buf_ctl_block_t *bcb = &buf_mgr.frames[frame_idx];
+  bcb->frame = page;
+  bcb->is_dirty = true;
+  bcb->ref_bit = true;
+}
+
+header_page_t *read_header_page(int fd, tableid_t table_id) {
+  page_t *header_page_buff = read_buffer(fd, table_id, HEADER_PAGE_POS);
+  header_page_t *header_page_ptr = (header_page_t *)&header_page_buff;
+}
+
+/**
+ * PREFETCH_SIZE만큼 더 페이지를 미리 버퍼에 가져오는 함수
+ */
+void prefetch(int fd, pagenum_t page_num, tableid_t table_id,
+              frame_idx_t frame_idx,
+              std::unordered_map<pagenum_t, frame_idx_t> &frame_mapper) {
+  header_page_t *header_page_ptr = read_header_page(fd, table_id);
+  int total_pages = header_page_ptr->num_of_pages;
+
+  for (int index = 1; index <= PREFETCH_SIZE; index++) {
+    pagenum_t prefetched_page_num = page_num + index;
+
+    // invalid
+    if (prefetched_page_num >= total_pages) {
+      break;
+    }
+
+    // if already exists in buffer
+    if (frame_mapper.count(prefetched_page_num)) {
+      continue;
+    }
+
+    // valid case
+    frame_idx_t prefetched_index =
+        find_free_frame_index(fd, table_id, page_num);
+
+    page_t *frame_ptr = (page_t *)buf_mgr.frames[prefetched_index].frame;
+    file_read_page(fd, prefetched_page_num, frame_ptr);
+    buf_mgr.page_table[table_id].insert(
+        std::make_pair(prefetched_page_num, prefetched_index));
+    set_new_prefetched_bcb(table_id, page_num, index, frame_ptr);
+  }
+}
+
+void set_new_bcb(tableid_t table_id, pagenum_t page_num, frame_idx_t frame_idx,
+                 page_t *page_buf) {
+  buf_mgr.frames[frame_idx].frame = page_buf;
+  buf_mgr.frames[frame_idx].table_id = table_id;
+  buf_mgr.frames[frame_idx].page_num = page_num;
+  buf_mgr.frames[frame_idx].is_dirty = false;
+  buf_mgr.frames[frame_idx].pin_count = 1;
+  buf_mgr.frames[frame_idx].ref_bit = true;
+}
+
+void set_new_prefetched_bcb(tableid_t table_id, pagenum_t page_num,
+                            frame_idx_t frame_idx, page_t *page_buf) {
+  buf_mgr.frames[frame_idx].frame = page_buf;
+  buf_mgr.frames[frame_idx].table_id = table_id;
+  buf_mgr.frames[frame_idx].page_num = page_num;
+  buf_mgr.frames[frame_idx].is_dirty = false;
+  buf_mgr.frames[frame_idx].pin_count = 0;
+  buf_mgr.frames[frame_idx].ref_bit = true;
+}
+
+frame_idx_t load_page_into_buffer(int fd, tableid_t table_id,
+                                  pagenum_t page_num) {
+  frame_idx_t frame_idx = find_free_frame_index(fd, table_id, page_num);
+
+  page_t *frame_ptr = (page_t *)buf_mgr.frames[frame_idx].frame;
+  file_read_page(fd, page_num, frame_ptr);
+
+  buf_mgr.page_table[table_id].insert(std::make_pair(page_num, frame_idx));
+
+  set_new_bcb(table_id, page_num, frame_idx, frame_ptr);
+
+  return frame_idx;
+}
+
+allocated_page_info_t make_and_pin_page(int fd, tableid_t table_id) {
+  pagenum_t page_num = file_alloc_page(fd);
+  if (page_num == PAGE_NULL) {
+    perror("Node creation.");
+    exit(EXIT_FAILURE);
+  }
+
+  frame_idx_t frame_idx = find_free_frame_index(fd, table_id, page_num);
+  page_t *frame_ptr = (page_t *)buf_mgr.frames[frame_idx].frame;
+  buf_mgr.page_table[table_id].insert(std::make_pair(page_num, frame_idx));
+
+  set_new_bcb(table_id, page_num, frame_idx, frame_ptr);
+
+  return {frame_ptr, page_num};
+}
+
+/**
+ * clock alogorithm---------------------------------------------------
+ */
+
+void update_clock_hand() {
+  buf_mgr.clock_hand = (buf_mgr.clock_hand + 1) % buf_mgr.frames_size;
+}
+
+/**
+ * clock algorithm main function
+ * frame배열에서 빈 페이지를 가져온다
+ */
+frame_idx_t find_free_frame_index(int fd, tableid_t table_id,
+                                  pagenum_t page_num) {
+  while (true) {
+    buf_ctl_block_t *bcb = &buf_mgr.frames[buf_mgr.clock_hand];
+    // Case: if used, skip
+    if (bcb->pin_count > 0) {
+      update_clock_hand();
+      continue;
+    }
+    // Case: if not used and not ref, found target
+    if (!bcb->ref_bit) {
+      // dirty page must be written
+      if (bcb->is_dirty) {
+        file_write_page(fd, bcb->page_num, (page_t *)bcb->frame);
+      }
+
+      // if other page already exists, eviction
+      if (bcb->page_num != 0) {
+        buf_mgr.page_table[bcb->table_id].erase(bcb->page_num);
+      }
+
+      frame_idx_t target_idx = buf_mgr.clock_hand;
+      update_clock_hand();
+      return target_idx;
+    }
+
+    // Case: give chance
+    bcb->ref_bit = false;
+    update_clock_hand();
+  }
+}
+
+/**
+ * set/unset pin count---------------------------------------------------
+ */
+
+void pin(tableid_t table_id, pagenum_t page_num) {
+  auto it = buf_mgr.page_table[table_id].find(page_num);
+
+  if (it != buf_mgr.page_table[table_id].end()) {
+    frame_idx_t frame_idx = it->second;
+    pin_frame(frame_idx);
+  }
+}
+
+void pin_frame(frame_idx_t frame_idx) {
+  buf_ctl_block_t *bcb = &buf_mgr.frames[frame_idx];
+
+  bcb->pin_count += 1;
+  bcb->ref_bit = true;
+}
+
+void unpin(tableid_t table_id, pagenum_t page_num) {
+  auto it = buf_mgr.page_table[table_id].find(page_num);
+
+  if (it != buf_mgr.page_table[table_id].end()) {
+    frame_idx_t frame_idx = it->second;
+    unpin_frame(frame_idx);
+  }
+}
+
+void unpin_frame(frame_idx_t frame_idx) {
+  buf_ctl_block_t *bcb = &buf_mgr.frames[frame_idx];
+
+  int next_pin_count = bcb->pin_count - 1;
+  if (next_pin_count < 0) {
+    next_pin_count = 0;
+  }
+  bcb->pin_count = next_pin_count;
+}
