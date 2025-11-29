4a79ab15bb76698e8f00b0faadced422d2f58b1e
hosigi7171-ux <hosigi7171@gmail.com>
2025-11-29 09:51:25 +0900

    refactor: remove unnecessary comments

--- bpt/include/file.h
+++ bpt/include/file.h
@@ -4,13 +4,9 @@
 #include "common_config.h"
 #include "page.h"
 
-// Allocate an on-disk page from the free page list
 pagenum_t file_alloc_page(int fd);
-// Free an on-disk page to the free page list
 void file_free_page(int fd, pagenum_t pagenum);
-// Read an on-disk page into the in-memory page structure(dest)
 void file_read_page(int fd, pagenum_t pagenum, page_t *dest);
-// Write an in-memory page(src) to the on-disk page
 void file_write_page(int fd, pagenum_t pagenum, const page_t *src);
 
 #endif
